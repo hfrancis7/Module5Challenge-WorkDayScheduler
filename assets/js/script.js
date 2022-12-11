ad33cca8f0fab65fb9945e16ccf41a0117358092
Hailey Francis <n01402670@unf.edu>
2022-12-11 14:54:25 -0500

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,51 @@
 $(function () {
+  //variable declarations
   let curDateEl = $("#currentDay");
   let saveBtnEl = $(".saveBtn");
   let container = $(".container-lg");
   let now = dayjs();
   
+  //save function linked to clicking save button
   saveBtnEl.click(save);
   
+  //load page on refresh
   load();
-
-setInterval(function(){
-    now = dayjs();
-    applyClasses();
-    displayTime();
-  }, 1000);
   
+  //loads all elements needed on page on refresh, starts interval
+  function load(){
+    //applies the classes and displays the time immediately before interval starts
+    applyClasses();
+    displayTime();  
+
+    setInterval(function(){ //display time every second and updates display classes
+        now = dayjs(); 
+        applyClasses();
+        displayTime();
+      }, 1000);
+      
+    //cycles through local storage and sets the textarea values to the previous saved values
+    Object.keys(localStorage).forEach((key) => {
+        let keyStr = "#" + key;
+        let item = localStorage.getItem(key);
+        $(keyStr).children("textarea").val(item);
+    })
+
+  }
+
+  //function that displays time on page
   function displayTime() {
     let nowText = now.format('MMM DD, YYYY [at] hh:mm:ss a');
     curDateEl.text(nowText);
   }
 
-  function load(){
-      applyClasses();
-      displayTime();
-      Object.keys(localStorage).forEach((key) => {
-        let keyStr = "#" + key;
-        let item = localStorage.getItem(key);
-        $(keyStr).children("textarea").val(item);
-      })
-
-  }
-
+  //saves the current textarea value that the button is associated with to local storage
   function save(){
     let input = $(this).siblings("textarea").val();
     let id = $(this).parent().attr("id");
     localStorage.setItem(id, input);
   }
 
+  //applies the classes past, present, and future based on the current hour
   function applyClasses(){
     let offset = 9;
     let hour = now.hour();
